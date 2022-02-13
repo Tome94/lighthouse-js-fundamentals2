3b3753cc3325c3b947a4cbb2d7fb0bcdacf4aa81
Tome94 <tommy_yang621@hotmail.com>
2022-02-13 02:43:07 +0000

    more shti

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+const ageCalculator = function (name, yearOfBirth, currentYear){
+  let age = 0
+  age = currentYear - yearOfBirth
+  console.log (name +' is ' + age + " years old.")
+}
+console.log (ageCalculator("Tommy", 1994, 2022))
+console.log (ageCalculator("Shiva", 1997, 2022))
---
+++ x-marks-the-spot.js
@@ -0,0 +1,42 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+//const finalShot = [-1 ,4]
+function finalPosition (moves){
+let x = 0;
+let y = 0 ;
+for (const move of moves){
+  if (move ==='north'){
+    y++;
+  }
+  else if (move === 'south'){
+    y--;
+  }
+  else if (move === 'west'){
+    x--;
+  }
+  else if (move === 'east'){
+    x++;
+  }
+}
+return [x ,y]
+}
+console.log (finalPosition (moves))
+
+/*
+function printGrid(x ,y){
+  const gridX = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+  const gridY = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+for (let i = 0; i <= gridX.length; i++){
+  let x = -5
+  x === x + gridX [i]
+  console.log (x)
+}
+ for (const x of gridX){
+    //return (x)
+    console.log (x)
+  }
+  for (const y of gridY){
+    //return (y)
+  } */
+
+//console.log (printGrid(x +" "+y))
+
