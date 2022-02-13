d68a0400191c80f289b9116e9f931e5ec9e84a44
Tome94 <tommy_yang621@hotmail.com>
2022-02-13 14:34:33 +0000

    fix the undefined line for age calculator

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,7 +1,7 @@
 const ageCalculator = function (name, yearOfBirth, currentYear){
   let age = 0
   age = currentYear - yearOfBirth
-  console.log (name +' is ' + age + " years old.")
+  return (name +' is ' + age + " years old.")
 }
 console.log (ageCalculator("Tommy", 1994, 2022))
-console.log (ageCalculator("Shiva", 1997, 2022))
+console.log (ageCalculator("Shiva", 1997, 2022))    
